a3f8a3fac0bc9f5ce1ac0e40f358847d71b8c23c
larry meyers <larry meyers>
2017-03-07 15:43:04 -0800

    Added id and key.

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package csci152
 
 import (
-		"html/template"
-		"net/http"
+	"html/template"
+	// "log"
+	"net/http"
+
+	"github.com/zmb3/spotify"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
 func init() {
@@ -12,7 +17,13 @@ func init() {
 	configureResourceLocation("js", "js")
 	setUserDefault()
 	// http.Handle("/favicon.ico", http.NotFoundHandler()) // ignore favicon request (error 404)
+
+	auth = spotify.NewAuthenticator(retrieveURI, spotify.ScopeUserReadPrivate)
+	auth.SetAuthInfo(clientID, spotKey)
+	baseURL = auth.AuthURL(spotStateValue)
+
 	http.HandleFunc("/", pageMain) // main page.
+	http.HandleFunc("/callback", completeAuth)
 
 	tpl = template.Must(template.ParseGlob("html/*.html"))
 }
@@ -24,4 +35,14 @@ func configureResourceLocation(phyDir, hrefDir string) {
 	fs := http.FileServer(http.Dir(phyDir))
 	fs = http.StripPrefix("/"+hrefDir, fs)
 	http.Handle("/"+hrefDir+"/", fs)
+}
+
+func completeAuth(res http.ResponseWriter, req *http.Request) {
+	ctx := appengine.NewContext(req)
+	_, err := auth.Token(spotStateValue, req)
+	if err != nil {
+		http.Error(res, "Couldn't get token", http.StatusForbidden)
+		log.Errorf(ctx, "Error %v", err)
+	}
+	log.Infof(ctx, "Created")
 }
--- variables.go
+++ variables.go
@@ -2,12 +2,15 @@ package csci152
 
 import (
 	"html/template"
+
+	"github.com/zmb3/spotify"
 )
 
 var tpl *template.Template              // html web page processing / parsing object
 var userInformation userInformationType // logged in user's information and preferences.
-// var movieAPI *tmdbgae.TMDb              // movie / tv database access object instance.
-var baseURL string						// base url to get images.
-var webInformation = webInformationType {
+// var movieAPI *tmdbgae.TMDb              	// movie / tv database access object instance.
+var baseURL string // base url to get images.
+var auth spotify.Authenticator
+var webInformation = webInformationType{
 	User: &userInformation,
 }
