ee757d4242dc346cc9ae030d97c041ba062ac2f9
larry meyers <larry meyers>
2017-04-29 10:16:34 -0700

    Moved images. Moved from trial directory to main image directory.

--- spotify.go
+++ spotify.go
@@ -1,7 +1,6 @@
 package csci152
 
 import (
-	"flag"
 	"net/http"
 
 	"google.golang.org/appengine"
@@ -27,8 +26,10 @@ func completeAuthentication(res http.ResponseWriter, req *http.Request) {
 }
 
 func initSpotify(res http.ResponseWriter, req *http.Request) {
+	ctx := appengine.NewContext(req)
+	log.Infof(ctx, "Spot login state: %v", webInformation.User.SpotLogged)
 	if !webInformation.User.SpotLogged {
-		auth = spotify.NewAuthenticator(retrieveURI, spotify.ScopeUserReadPrivate)
+		auth = spotify.NewAuthenticator(retrieveURI, spotify.ScopeUserReadRecentlyPlayed)
 		auth.SetAuthInfo(clientID, spotKey)
 		http.Redirect(res, req, auth.AuthURL(spotStateValue), http.StatusFound)
 	}
@@ -36,32 +37,23 @@ func initSpotify(res http.ResponseWriter, req *http.Request) {
 
 // check if client object has been defined; returns true if Ok..
 func clientOK() bool {
-	return (spotClient == spotify.Client{})
+	return !(spotClient == spotify.Client{})
 }
 
 func loadPlayLists(res http.ResponseWriter, req *http.Request) {
-	flag.Parse()
-
 	ctx := appengine.NewContext(req)
 	log.Infof(ctx, "In playlist")
 	if clientOK() {
-		log.Infof(ctx, "Client Empty")
-	} else {
-		log.Infof(ctx, "Client not empty")
+		log.Infof(ctx, "Client Not Empty")
 
 		user, _ := spotClient.CurrentUser()
-		// user, _ := spotify.GetUsersPublicProfile(spotify.ID(*userID))
 		log.Infof(ctx, "User ID: %v", user.ID)
-		// log.Infof(ctx, "Display name: %v", user.DisplayName)
-		// log.Infof(ctx, "Spotify URI: %v", string(user.URI))
-		// log.Infof(ctx, "Endpoint: %v", user.Endpoint)
-		// log.Infof(ctx, "Followers: %v", user.Followers.Count)
+		//
 		sr, err := spotClient.CurrentUsersPlaylists()
-		// sr, err := spotClient.GetPlaylistsForUser(user.ID)
+		srx, err := spotClient.CurrentUserRecentTracks(5)
+		//
 		log.Infof(ctx, "Playlist (full) ==> %v", sr)
 		if err == nil {
-			// trks := sr.Playlists[0].Name
-			// log.Infof(ctx, "\nPlaylist (track) ==> %v\n", sr.Playlists[0].ID)
 			for _, val := range sr.Playlists {
 				trk, err := spotClient.GetPlaylistTracks(user.ID, val.ID)
 				if err == nil {
@@ -69,19 +61,27 @@ func loadPlayLists(res http.ResponseWriter, req *http.Request) {
 						log.Infof(ctx, "\nPlaylist (track) ==> %v\n", tVal.Track.Name)
 					}
 				}
-				// trk, _ := spotClient.GetTrack(val.ID)
-				// log.Infof(ctx, "\nPlaylist (track) ==> %v\n", val.ID)
-				// trk, err := spotClient.GetTrack(val.ID)
 				if err != nil {
 					log.Infof(ctx, "Error")
 				}
 			}
-			// 	// for _, val := range trks {
-			// 	// 	log.Infof(ctx, "Tracks=%v", val.Name)
-			// 	// }
+			log.Infof(ctx, "Items ==> %v", srx.Items)
+			// for _, val := range srx.Items {
+			// 	trk, err := spotClient.GetPlaylistTracks(user.ID, val.ID)
+			// 	if err == nil {
+			// 		for _, tVal := range trk.Tracks {
+			// 			log.Infof(ctx, "\nPlaylist (track) ==> %v\n", tVal.Track.Name)
+			// 		}
+			// 	}
+			if err != nil {
+				log.Infof(ctx, "Error")
+				// }
+			}
 		} else {
 			log.Infof(ctx, "Error %v", err)
 		}
+	} else {
+		log.Infof(ctx, "Client Empty")
 	}
 	http.Redirect(res, req, "/home", http.StatusSeeOther)
 
