61f2d99ac2fa0acc2787d2bcdd6a135344487212
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2017-05-07 00:17:11 -0700

    Add playlist name constant

--- constants.go
+++ constants.go
@@ -1,10 +1,11 @@
 package csci152
 
-const dataKind string = "CSCI152"                           // user information datastore name.
-const nameDict string = "Dictionary"                        // dictionary containing taken usernames.
-const passwordPrefix string = "^%s5G"                       // password prefix salt
-const passwordSuffix string = "fk4%S"                       // password suffix salt
-const clientID string = "d6d93dec27944b758798ff73ea0a4598"  // client idenifier key.
-const spotKey string = "0a1f460af14047afa96e2b5d6816f109"   // key.
-const retrieveURI string = "http://localhost:8080/callback" // retrieve information callback URL.
-const spotStateValue string = "DevTest"                     // spotify session id / state value.
+const dataKind string = "CSCI152"                               // user information datastore name.
+const nameDict string = "Dictionary"                            // dictionary containing taken usernames.
+const passwordPrefix string = "^%s5G"                           // password prefix salt
+const passwordSuffix string = "fk4%S"                           // password suffix salt
+const clientID string = "d6d93dec27944b758798ff73ea0a4598"      // client idenifier key.
+const spotKey string = "0a1f460af14047afa96e2b5d6816f109"       // key.
+const retrieveURI string = "http://localhost:8080/callback"     // retrieve information callback URL.
+const spotStateValue string = "DevTest"                         // spotify session id / state value.
+const playlistNameConst string = "Your Personalized Taste Test" // constant playlist creation name
